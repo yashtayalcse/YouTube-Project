afb09a7990b0c3cc5b64c61c093c33593250aee6
yashtayalcse <yashtayal28763@gmail.com>
2026-06-15 21:20:50 +0530

    implement healthcheck controller to return OK status as JSON

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -5,6 +5,7 @@ import asyncWrapper from "../utils/asyncWrapper.js"
 
 const healthcheck = asyncWrapper(async (req, res) => {
     //TODO: build a healthcheck response that simply returns the OK status as json with a message
+    return res.status(200).json(new ApiResponse(200, "Health check passed",{status: "Ok"}))
 })
 
 export {
